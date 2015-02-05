648252144f4fd3f3bca3f81262d837a20a7bbb66
Caroline <caroline@uber.com>
2015-02-04 21:08:11 -0800

    Add initial index and explore pages.

---
+++ README.md
@@ -0,0 +1,10 @@
+mac@ ~/uber-explore  » virtualenv env
+New python executable in env/bin/python
+Installing setuptools, pip...done.
+
+mac@ ~/uber-explore  » . env/bin/activate
+
+(env)mac@ ~/uber-explore  » pip install flask
+
+(env)mac@ ~/uber-explore  » python application.py
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
---
+++ application.py
@@ -0,0 +1,61 @@
+import urllib
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# client id from developer.uber.com/apps
+CLIENT_ID = ''
+
+WECODE_LATITUDE = 42.3799673
+WECODE_LONGITUDE = 71.1156968
+
+MUBER_URL = 'http://m.uber.com/sign-up?'
+
+
+@app.route('/')
+def index():
+    """Render the start page."""
+    print 'Rendering index.html'
+    return render_template('index.html')
+
+@app.route('/explore')
+def explore():
+    """Render a random location and Uber button."""
+    # Fetch all locations
+
+
+    # Pick a random location
+
+
+    # Parse variables for page
+    destination = ''
+    dropoff_latitude = 0
+    dropoff_longitude = 0
+
+    time_estimate = 0 # minutes
+    price_estimate = ''
+
+    product_id = 'uuid'
+
+    #  Construct deeplinkurl to request a ride
+    request_params = {
+        'client_id': CLIENT_ID,
+        'product_id': product_id,
+        'pickup_latitude': WECODE_LATITUDE,
+        'pickup_longitude': WECODE_LONGITUDE,
+        'dropoff_latitude': dropoff_latitude,
+        'dropoff_longitude': dropoff_longitude,
+    }
+    request_url = MUBER_URL + urllib.urlencode(request_params)
+
+    # Render the page
+    return render_template(
+        'explore.html',
+        destination=destination,
+        time_estimate=time_estimate,
+        price_estimate=price_estimate,
+        request_url=request_url,
+    )
+
+if __name__ == '__main__':
+    app.run()
